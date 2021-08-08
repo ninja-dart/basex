09c434c2b65ef18fb161c086c55b134fcf063353
Ravi Teja Gudapati <tejainece@gmail.com>
2021-08-08 08:32:48 +0200

    toBaseBytes and toBase

---
+++ example/basex_example.dart
@@ -0,0 +1,42 @@
+import 'package:basex/basex.dart';
+import 'package:ninja/ninja.dart';
+
+void main() {
+  {
+    final out = List.filled(4, 0);
+    pack('Man'.codeUnits, 64, 63, out, 0);
+    print(out);
+  }
+
+  {
+    final out = toBaseBytes('Man'.codeUnits, 64);
+    print(out);
+  }
+
+  {
+    final out = toBaseBytes('Ma'.codeUnits, 64);
+    print(out);
+  }
+
+  {
+    final out = toBaseBytes('Ma'.codeUnits, 64, padding: 61);
+    print(out);
+  }
+
+  {
+    final out = toBase('Man'.codeUnits, base64Alphabet);
+    print(out);
+  }
+
+  {
+    final out = toBase('Ma'.codeUnits, base64Alphabet);
+    print(out);
+  }
+  
+  {
+    final bint = BigInt.parse('751e76e8199196d454941c45d1b3a323f1433bd6', radix: 16);
+    final input = bigIntToBytes(bint);
+    final out = toBaseBytes(input, 32);
+    print(out);
+  }
+}
